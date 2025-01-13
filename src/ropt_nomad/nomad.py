af5fa77b94b2dd787fabcf2e47147c50a864f6af
Peter Verveer <pieter.verveer@tno.nl>
2025-01-13 13:47:31 +0000

    Update plugin code

--- src/ropt_nomad/nomad.py
+++ src/ropt_nomad/nomad.py
@@ -374,7 +374,7 @@ class NomadOptimizerPlugin(OptimizerPlugin):
         """
         return NomadOptimizer(config, optimizer_callback)
 
-    def is_supported(self, method: str, *, explicit: bool) -> bool:  # noqa: ARG002
+    def is_supported(self, method: str) -> bool:
         """Check if a method is supported.
 
         See the [ropt.plugins.optimizer.base.OptimizerPlugin][] abstract base class.
