fba3537c80f3fc48ef3df85d7019be9350b97ca8
Peter Verveer <pieter.verveer@tno.nl>
2026-01-08 16:22:34 +0100

    Update the test code

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,9 @@ from typing import Any
 import numpy as np
 import pytest
 from numpy.typing import NDArray
-from ropt.evaluator import EvaluatorContext, EvaluatorResult
+from ropt.workflow import create_evaluator
 
-_Function = Callable[[NDArray[np.float64]], float]
+_Function = Callable[[NDArray[np.float64], int], float]
 
 
 def pytest_addoption(parser: Any) -> Any:
@@ -27,42 +27,8 @@ def pytest_collection_modifyitems(config: Any, items: Sequence[Any]) -> None:
                 item.add_marker(skip_external)
 
 
-def _function_runner(
-    variables: NDArray[np.float64],
-    evaluator_context: EvaluatorContext,
-    functions: list[_Function],
-) -> EvaluatorResult:
-    objective_count = evaluator_context.config.objectives.weights.size
-    constraint_count = (
-        0
-        if evaluator_context.config.nonlinear_constraints is None
-        else evaluator_context.config.nonlinear_constraints.lower_bounds.size
-    )
-    objective_results = np.zeros(
-        (variables.shape[0], objective_count), dtype=np.float64
-    )
-    constraint_results = (
-        np.zeros((variables.shape[0], constraint_count), dtype=np.float64)
-        if constraint_count > 0
-        else None
-    )
-    for eval_idx in range(evaluator_context.realizations.size):
-        if evaluator_context.active[eval_idx]:
-            for idx in range(objective_count):
-                function = functions[idx]
-                objective_results[eval_idx, idx] = function(variables[eval_idx, :])
-            for idx in range(constraint_count):
-                function = functions[idx + objective_count]
-                assert constraint_results is not None
-                constraint_results[eval_idx, idx] = function(variables[eval_idx, :])
-    return EvaluatorResult(
-        objectives=objective_results,
-        constraints=constraint_results,
-    )
-
-
 def _compute_distance_squared(
-    variables: NDArray[np.float64], target: NDArray[np.float64]
+    variables: NDArray[np.float64], _: int, target: NDArray[np.float64]
 ) -> float:
     return float(((variables - target) ** 2).sum())
 
@@ -75,9 +41,25 @@ def fixture_test_functions() -> tuple[_Function, _Function]:
     )
 
 
+def _function(
+    variables: NDArray[np.float64],
+    realization: int,
+    _: int,
+    /,
+    *,
+    test_functions: list[_Function],
+) -> NDArray[np.float64]:
+    return np.fromiter(
+        (func(variables, realization) for func in test_functions), dtype=np.float64
+    )
+
+
 @pytest.fixture(scope="session")
-def evaluator(test_functions: Any) -> Callable[[list[_Function]], Any]:
+def evaluator(test_functions: Any) -> Any:
     def _evaluator(test_functions: list[_Function] = test_functions) -> Any:
-        return partial(_function_runner, functions=test_functions)
+        return create_evaluator(
+            "function_evaluator",
+            function=partial(_function, test_functions=test_functions),
+        )
 
     return _evaluator
